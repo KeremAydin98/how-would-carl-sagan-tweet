cb2d7676c8aa6491a53c77964e2dd50e6a3c4f6f
KeremAydin98 <aydinke16@itu.edu.tr>
2022-10-19 09:20:41 +0300

    added new book

--- pdf2txt.py
+++ pdf2txt.py
@@ -1,25 +1,37 @@
+import os, string
 import PyPDF2
 
+all_text = ""
 # create file object variable
 # opening method will be rb
-pdffileobj = open('.Data/cosmos.pdf', 'rb')
+for file in os.listdir("Data/"):
+    if file.endswith(".pdf"):
+        pdffileobj = open(f'Data/{file}', 'rb')
 
-# create reader variable that will read the pdffileobj
-pdfreader = PyPDF2.PdfFileReader(pdffileobj)
+        # create reader variable that will read the pdffileobj
+        pdfreader = PyPDF2.PdfFileReader(pdffileobj)
 
-# This will store the number of pages of this pdf file
-x = pdfreader.numPages
+        # This will store the number of pages of this pdf file
+        x = pdfreader.numPages
 
-all_text = ""
-for i in range(4,x-4):
+        for i in range(4,x-4):
 
-    # create a variable that will select the selected number of pages
-    pageobj = pdfreader.getPage(i)
+            # create a variable that will select the selected number of pages
+            pageobj = pdfreader.getPage(i)
 
-    # create text variable which will store all text datafrom pdf file
-    text = pageobj.extractText()
+            # create text variable which will store all text datafrom pdf file
+            text = pageobj.extractText()
 
-    all_text = all_text + "\n" + text
+            text = text.encode("ascii","ignore")
+            text = text.decode()
+
+            punctuations = string.punctuation
+            punctuations.replace(",","")
+            punctuations.replace(".","")
+
+            text = text.translate(str.maketrans('', '', punctuations))
+
+            all_text = all_text + "\n" + text
 
 # Write all the text in one txt file
 with open("./Data/all_text.txt", "w") as f:
