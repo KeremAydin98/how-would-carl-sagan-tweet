2696d80da9558a540d9572a589bd7aed75953f83
KeremAydin98 <aydinke16@itu.edu.tr>
2022-10-20 09:37:14 +0300

    tweeting engaged

--- generate_tweets.py
+++ generate_tweets.py
@@ -1,8 +1,11 @@
+import string
+
 import numpy as np
 import tensorflow as tf
 import config
 import pickle
-
+from keybert import KeyBERT
+import random
 
 def predict_text(model, text, tokenizer, temperature=1):
     x = np.array(tokenizer.texts_to_sequences([text])) - 1
@@ -18,9 +21,10 @@ def predict_text(model, text, tokenizer, temperature=1):
     return tokenizer.sequences_to_texts(char_id.numpy())[0]
 
 
-def generate_text(model, tokenizer, first="e"):
+def generate_text(model, tokenizer):
 
-  text = first
+  text = random.choice(string.ascii_lowercase)
+
   for _ in range(280):
 
     text += predict_text(model, text, tokenizer)
@@ -34,7 +38,18 @@ model = tf.keras.models.load_model("./Models/best_model.h5")
 with open('./Models/tokenizer.pickle', 'rb') as handle:
     char_tokenizer = pickle.load(handle)
 
+for i in range(5):
 
-generated_text = generate_text(model, char_tokenizer)
+    # Generate the tweet of Carl Sagan
+    generated_text = generate_text(model, char_tokenizer)
 
-print(generated_text)
+    # Bert Keyphrase Extraction method
+    kw_model = KeyBERT()
+
+    # Keyword generation
+    keywords = kw_model.extract_keywords(generated_text, keyphrase_ngram_range=(1,1))
+
+    with open(f".Data/Tweet_{i}.txt","w") as f:
+
+        whole_tweet = generated_text + "@" + keywords[0] + "@" + keywords[1]
+        f.write(whole_tweet)
--- training.py
+++ training.py
@@ -14,11 +14,15 @@ train_dataset = window_data(dataset,window_size=101)
 # Load the model
 model = GRU_model()
 
+# Training the model
 model.fit(train_dataset.repeat(), epochs=10000, steps_per_epoch = 50)
 
-# saving
+# Saving tokenizer
 with open('./Models/tokenizer.pickle', 'wb') as handle:
     pickle.dump(char_tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# Saving model
+model.save("Models/best_model.h5")
 
 
 
