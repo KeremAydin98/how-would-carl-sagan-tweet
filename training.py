e6028eea8e7aaa4ec159788815e922c6c3fb8e3b
KeremAydın <77073029+KeremAydin98@users.noreply.github.com>
2022-11-01 10:31:05 +0300

    Update training.py

--- training.py
+++ training.py
@@ -15,7 +15,7 @@ train_dataset = window_data(dataset,window_size=101)
 model = GRU_model()
 
 # Training the model
-model.fit(train_dataset.repeat(), epochs=10000, steps_per_epoch = 50)
+model.fit(train_dataset, epochs=10000, steps_per_epoch=20)
 
 # Saving tokenizer
 with open('./Models/tokenizer.pickle', 'wb') as handle:
